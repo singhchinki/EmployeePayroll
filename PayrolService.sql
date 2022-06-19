8a4214869a9b8f52782def304e1c2badc92ec64f
chinki kumari singh <singhchinkikumari.com>
2022-06-19 17:08:58 +0530

    [Chinki] Extend Table for Basic Pay

--- PayrolService.sql
+++ PayrolService.sql
@@ -23,3 +23,19 @@ select AVG(Salary) as AverageSalary from employee_payrole group by Gender;
 select count(Salary) as TotalSalary,Gender from employee_payrole group by Gender;
 select Min(Salary) as MinSalary,Gender from employee_payrole group by Gender;
 select Max(Salary) as MaxSalary,Gender from employee_payrole group by Gender;
+
+alter table employee_payrole add PhoneNumber bigint;
+alter table employee_payrole add Department varchar(250) not null default 'HR';
+alter table employee_payrole add Address varchar(250) default 'bangaluru';
+select * from employee_payrole;
+
+---RenameColomn name in existing table(Salary renamed as Basic pay)---
+Exec sp_rename 'employee_payrole.Salary', 'BasicPay';
+alter table employee_payrole add TaxablePay float, Deduction float,IncomeTax float,NetPay float;
+Update employee_payrole set Deduction = '4000' where Department = 'HR';
+Update employee_payrole set Deduction = '3000' where Department = 'Sales';
+Update employee_payrole set Deduction = '2000' where Department = 'Customer Service';
+Update employee_payrole set NetPay = (BasicPay-Deduction);
+Update employee_payrole set TaxablePay = '1000';
+Update employee_payrole set IncomeTax = '200';
+select * from employee_payrole;
