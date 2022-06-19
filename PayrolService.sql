d9e5b7122796c4820e12bc4331825b39e6e8e4ab
chinki kumari singh <singhchinkikumari.com>
2022-06-19 13:54:08 +0530

    [Chinki] Insert Value in Table

--- PayrolService.sql
+++ PayrolService.sql
@@ -7,3 +7,5 @@ CREATE TABLE employee_payrole(
 	Salary float,
 	StartDate date
 	);
+
+	INSERT INTO employee_payrole VALUES('Abhishek','12345','2016/03/01') ,('Viahan','13335','2018/01/05'),('Virat','525245','2019/03/01');
