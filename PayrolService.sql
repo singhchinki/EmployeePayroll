5418ac2bccf3d41989e3bcb3fe5568de4bbf726d
chinki kumari singh <singhchinkikumari.com>
2022-06-20 16:34:29 +0530

    [Chinki] Ensure all retrieve queries done especially in UC4, UC5 and UC7 are working with new table structure

--- PayrolService.sql
+++ PayrolService.sql
@@ -121,3 +121,36 @@ select min(basic_pay) from employee where gender='F' group by gender;
 select max(basic_pay) from employee where gender='M' group by gender;
 select max(basic_pay) from employee where gender='F' group by gender;
 select count(phone_number) as 'number of gender' from employee group by gender;
+
+select * from employee e inner join company c on 
+e.company_id=c.company_id inner join payroll p on
+p.payroll_id=e.payroll_id inner join EmployeeDepartment ed on
+e.employee_id=ed.employee_Department_id inner join Department d on
+ed.employee_Department_id=d.department_id where e.employee_id=1;
+
+select * from employee e inner join company c on 
+e.company_id=c.company_id inner join payroll p on
+p.payroll_id=e.payroll_id inner join EmployeeDepartment ed on
+e.employee_id=ed.employee_Department_id inner join Department d on
+ed.employee_Department_id=d.department_id where e.employee_id=1;
+e.employee_id=ed.employee_id inner join Department d on
+ed.department_id=d.department_id where e.employee_id=1;
+
+select * from employee e inner join company c on 
+e.company_id=c.company_id inner join payroll p on
+p.payroll_id=e.payroll_id inner join EmployeeDepartment ed on
+e.employee_id=ed.employee_id inner join Department d on
+ed.department_id=d.department_id where e.employee_id=2;
+
+/*UC-5*/
+select basic_pay from employee where start_date between CAST('2020-10-2' as date) and GETDATE();
+ed.department_id=d.department_id where e.employee_id=1;
+
+select * from employee e inner join company c on 
+e.company_id=c.company_id inner join payroll p on
+p.payroll_id=e.payroll_id inner join EmployeeDepartment ed on
+e.employee_id=ed.employee_id inner join Department d on
+ed.department_id=d.department_id where e.employee_id=2;
+
+/*UC-5*/
+select basic_pay from employee where start_date between CAST('2020-10-2' as date) and GETDATE();
